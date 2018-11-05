7bcbff63afe7d39d13b2438ad77e1abd6c5b4eb4
David Šibrava <sibradav@fel.cvut.cz>
2018-11-05 12:11:43 +0100

    Added html webpage construction.

--- GUITest/GUI.h
+++ GUITest/GUI.h
@@ -4,12 +4,12 @@
 #include "GUIElement.hpp"
 #include <vector>
 
-class GUI 
+class GUI
 {
 public:
 	GUI()
 	{
-		
+
 	}
 	/**
 	Send the text out using Websockets
@@ -19,6 +19,45 @@ public:
 
 		return 1;
 	}
+	String getHTML()
+	{
+		String returnString;
+		returnString += getHeader();
+		for (std::vector<int>::size_type i = 0; i != elements.size(); i++) {
+			{
+				returnString+=elements[i].getHTML();
+			}
+		}
+		returnString += getFooter();
+	}
+	String getHeader()
+	{
+		return "<!DOCTYPE html>\n\
+<html>\n\
+\n";
+	}
+	String getFooter()
+	{
+		return "</html> \n";
+	}
+
+	String getScript()
+	{
+		return " <head>\n\
+<script>\n\
+var theSocket = new WebSocket(\"ws://192.168.0.102:81\", [\'arduino\']);\n\
+\n\
+function sendJSON(theJSON)\n\
+{\n\
+  theSocket.send(JSON.stringify(theJSON));\n\
+}\n\
+\n\
+theSocket.onmessage = function (event) {\n\
+  console.log(event.data);\n\
+}\n\
+</script>\n\
+</head>";
+	}
 
 	void add(GUIElement ge)
 	{
@@ -27,13 +66,13 @@ public:
 	}
 	GUIElement* find(String s)
 	{
-			for(std::vector<int>::size_type i = 0; i != elements.size(); i++) {
-				if (elements[i].getId() == s)
-				{
-					return &elements[i];
-				}
+		for (std::vector<int>::size_type i = 0; i != elements.size(); i++) {
+			if (elements[i].getId() == s)
+			{
+				return &elements[i];
 			}
-			return NULL;
+		}
+		return NULL;
 	}
 
 	int handleRequest(JsonObject obj)
@@ -53,7 +92,7 @@ public:
 		}
 		return 0;
 	}
-	
+
 private:
 	std::vector<GUIElement> elements;
 };
